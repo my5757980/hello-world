4e0ca745385d27944bc1cc882ee4d78083c369c9
Maira Naeem <163671644+mairanoor412@users.noreply.github.com>
2024-09-20 09:32:47 -0700

    push my code to github

---
+++ src/app/jobs/font/page.tsx
@@ -0,0 +1,9 @@
+function Index(){
+    return(
+      <div>
+        <h1>hello hanzala</h1>
+      </div>
+    )
+  }
+  
+  export default Index
---
+++ src/app/jobs/page.tsx
@@ -0,0 +1,9 @@
+function Global(){
+    return(
+      <div>
+        <h1>hello yaseen</h1>
+      </div>
+    )
+  }
+  
+  export default Global
